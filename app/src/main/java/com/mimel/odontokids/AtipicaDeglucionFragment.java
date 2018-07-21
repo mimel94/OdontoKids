850fa16be786aaa48d9ff7496cc7c55493aa713f
mimel <millergarciaq@gmail.com>
2018-07-21 14:51:22 -0500

    modified list view of template

--- app/src/main/java/com/mimel/odontokids/AtipicaDeglucionFragment.java
+++ app/src/main/java/com/mimel/odontokids/AtipicaDeglucionFragment.java
@@ -13,15 +13,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.MediaController;
-import android.widget.TextView;
+import android.widget.Spinner;
 import android.widget.Toast;
 import android.widget.VideoView;
-
-import java.lang.reflect.Array;
 
 import utilidades.Utilidades;
 
@@ -36,7 +33,7 @@ public class AtipicaDeglucionFragment extends Fragment {
         // Required empty public constructor
     }
 
-    private ListView lv1;
+    private Spinner options;
     private VideoView videoView;
     private ImageView imageLogo;
 
@@ -51,15 +48,88 @@ public class AtipicaDeglucionFragment extends Fragment {
         // Inflate the layout for this fragment
         final View rootView = inflater.inflate(R.layout.fragment_atipica_deglucion, container, false);
 
-        lv1 = (ListView) rootView.findViewById(R.id.listPraxias);
+        options = (Spinner) rootView.findViewById(R.id.listPraxias);
         videoView = (VideoView) rootView.findViewById(R.id.videoView);
         imageLogo = (ImageView)rootView.findViewById(R.id.imageView2);
 
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item_therapy, nameTherapy );
-        lv1.setAdapter(adapter);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(),R.array.videos_deglution_atipical, android.R.layout.simple_spinner_dropdown_item);
+        options.setAdapter(adapter);
 
-        lv1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        options.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                imageLogo.setVisibility(View.INVISIBLE);
+                videoView.setVisibility(View.VISIBLE);
+                if(i == 0){
+                    imageLogo.setVisibility(View.VISIBLE);
+                    videoView.setVisibility(View.INVISIBLE);
+                }else if (i == 1){
+                    String videoPath = "android.resource://"+getActivity().getPackageName() + "/" + R.raw.masajeconmanos;
+                    Uri uri = Uri.parse(videoPath);
+                    videoView.setVideoURI(uri);
+
+
+                }else if (i == 2){
+                    String videoPath = "android.resource://"+getActivity().getPackageName() + "/" + R.raw.masajeador;
+                    Uri uri = Uri.parse(videoPath);
+                    videoView.setVideoURI(uri);
+                }else if (i == 3){
+                    String videoPath = "android.resource://"+getActivity().getPackageName() + "/" + R.raw.cepilladoconlalengua;
+                    Uri uri = Uri.parse(videoPath);
+                    videoView.setVideoURI(uri);
+
+                }else if (i == 4){
+                    String videoPath = "android.resource://"+getActivity().getPackageName() + "/" + R.raw.cauchito;
+                    Uri uri = Uri.parse(videoPath);
+                    videoView.setVideoURI(uri);
+                }else if (i == 5){
+                    String videoPath = "android.resource://"+getActivity().getPackageName() + "/" + R.raw.dulce;
+                    Uri uri = Uri.parse(videoPath);
+                    videoView.setVideoURI(uri);
+                }else if (i == 6){
+                    String videoPath = "android.resource://"+getActivity().getPackageName() + "/" + R.raw.aoi;
+                    Uri uri = Uri.parse(videoPath);
+                    videoView.setVideoURI(uri);
+                }
+                else if (i == 7){
+                    String videoPath = "android.resource://"+getActivity().getPackageName() + "/" + R.raw.moverlabios;
+                    Uri uri = Uri.parse(videoPath);
+                    videoView.setVideoURI(uri);
+                }else if (i == 9){
+                    String videoPath = "android.resource://"+getActivity().getPackageName() + "/" + R.raw.tarjeta;
+                    Uri uri = Uri.parse(videoPath);
+                    videoView.setVideoURI(uri);
+                }else if (i == 8){
+                    String videoPath = "android.resource://"+getActivity().getPackageName() + "/" + R.raw.inflarbomba;
+                    Uri uri = Uri.parse(videoPath);
+                    videoView.setVideoURI(uri);
+                }else if (i == 11){
+                    String videoPath = "android.resource://"+getActivity().getPackageName() + "/" + R.raw.lalala;
+                    Uri uri = Uri.parse(videoPath);
+                    videoView.setVideoURI(uri);
+                }else if (i == 10){
+                    String videoPath = "android.resource://"+getActivity().getPackageName() + "/" + R.raw.caballo;
+                    Uri uri = Uri.parse(videoPath);
+                    videoView.setVideoURI(uri);
+                }else if (i == 12){
+                    String videoPath = "android.resource://"+getActivity().getPackageName() + "/" + R.raw.circuloconlalengua;
+                    Uri uri = Uri.parse(videoPath);
+                    videoView.setVideoURI(uri);
+                }else{
+                    Toast.makeText(getActivity(),"Que estraño esta opción no esta :O", Toast.LENGTH_SHORT).show();
+                }
+                videoView.start();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+
+        /*options.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 imageLogo.setVisibility(View.INVISIBLE);
@@ -122,7 +192,7 @@ public class AtipicaDeglucionFragment extends Fragment {
                 videoView.start();
 
             }
-        });
+        });*/
         MediaController mediaController = new MediaController(getActivity());
         videoView.setMediaController(mediaController);
         mediaController.setAnchorView(videoView);
